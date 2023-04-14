5fbd0542227c9d7920bdac4d2d3ed6d1485e4805
iam6adman <iam6adman@outlook.com>
2023-04-14 18:58:48 +0300

    fix Bug Example 010

--- Example010/Program.cs
+++ Example010/Program.cs
@@ -1,20 +1,20 @@
-﻿// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем. 
+﻿// Функция принимает сообщение для отображения, и выводит результат.
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
-// Функция вывода цифры стоящей в середине трехзначногно числа.
+// Функция вывода цифры стоящей в середине.
 int InCenter(int a)
 {
     
     int result = ((a / 10) % 10);
     return result;
 }
-int number = ReadInt("Введите трехзначное число: ");
+int number = ReadInt("Введите трехзначное число");
 int amount = number.ToString().Length;
 
-if (amount < 3 || amount > 3)
+if (amount < 3 && amount > 3)
 {
     Console.WriteLine("Вы ввели не трехзначное число");
 }
